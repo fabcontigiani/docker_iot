a4879e2b7acd345be05c40ad857ecc5812962068
Fabrizio Contigiani <fabcontigiani@gmail.com>
2025-04-16 15:30:50 -0300

    Agregar captura de excepción al detener con ctrl-c

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -68,4 +68,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Programa detenido por el usuario (Ctrl+C)")
