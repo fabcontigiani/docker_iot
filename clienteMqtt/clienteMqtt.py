1be340d6c8633ca17976b94a3b236cb660c4d1c1
Fabrizio Contigiani <fabcontigiani@gmail.com>
2025-05-21 13:26:07 -0300

    format

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,10 +1,22 @@
-import asyncio, ssl, certifi, logging, os, aiomysql, json, traceback
-import aiomqtt
+import asyncio
+import json
+import logging
+import os
+import ssl
+import traceback
 
-logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
+import aiomqtt
+import aiomysql
+import certifi
+
+logging.basicConfig(
+    format="%(asctime)s - cliente mqtt - %(levelname)s:%(message)s",
+    level=logging.INFO,
+    datefmt="%d/%m/%Y %H:%M:%S %z",
+)
+
 
 async def main():
-
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
@@ -17,17 +29,20 @@ async def main():
         port=int(os.environ["PUERTO_MQTTS"]),
         tls_context=tls_context,
     ) as client:
-        await client.subscribe(os.environ['TOPICO'] + '/#')
+        await client.subscribe(os.environ["TOPICO"] + "/#")
         async for message in client.messages:
             logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
-            dispositivo=str(message.topic).split('/')[-1]
-            datos=json.loads(message.payload.decode('utf8'))
+            dispositivo = str(message.topic).split("/")[-1]
+            datos = json.loads(message.payload.decode("utf8"))
             sql = "INSERT INTO `mediciones` (`sensor_id`, `temperatura`, `humedad`) VALUES (%s, %s, %s)"
             try:
-                conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
-                                            user=os.environ["MARIADB_USER"],
-                                            password=os.environ["MARIADB_USER_PASS"],
-                                            db=os.environ["MARIADB_DB"])
+                conn = await aiomysql.connect(
+                    host=os.environ["MARIADB_SERVER"],
+                    port=3306,
+                    user=os.environ["MARIADB_USER"],
+                    password=os.environ["MARIADB_USER_PASS"],
+                    db=os.environ["MARIADB_DB"],
+                )
             except Exception as e:
                 logging.error(traceback.format_exc())
 
@@ -35,13 +50,16 @@ async def main():
 
             async with conn.cursor() as cur:
                 try:
-                    await cur.execute(sql, (dispositivo, datos['temperatura'], datos['humedad']))
+                    await cur.execute(
+                        sql, (dispositivo, datos["temperatura"], datos["humedad"])
+                    )
                     await conn.commit()
                     await cur.close()
                     await conn.ensure_closed()
                 except Exception as e:
                     logging.error(traceback.format_exc())
-                    # Logs the error appropriately. 
+                    # Logs the error appropriately.
+
 
 if __name__ == "__main__":
     asyncio.run(main())
